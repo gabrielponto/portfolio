f4c8a4a1f461e1e0df30e91bb10a30dc1545da14
Gabriel Oliveira <dev@gabrieloliveira.net>
2016-04-09 16:39:23 -0300

    faz funcionar o contato

--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Block, Case, BlockCase, Skill
+from django.core.mail import EmailMessage
 
-def index(request):
+def index(request, contact=False):
     blocks = Block.objects.order_by('position')
     skills = Skill.objects.order_by('-rating')
-    context = {'blocks': blocks, 'skills': skills}
+    context = {'blocks': blocks, 'skills': skills, 'contact': False}
+    if contact:
+        context['contact'] = True
     return render(request, 'main/index.html', context)
 
 def case(request, name):
@@ -13,4 +16,13 @@ def case(request, name):
     return render(request, 'main/case.ajax.html', context)
 
 def contact(request):
-    return render(request, 'main/index.html')
+    post = request.POST
+    if post:
+        message = u'<p>' + post.get('name') + u' (' + post.get('email') + u') enviou a seguinte mensagem: </p>'
+        message += u'<p>' + post.get('message') + u'</p>'
+        email = EmailMessage('Contato Recebido pelo Form do Site', message, to=['gabriellucianooliveira@gmail.com'])
+        email.send()
+        ctx = True
+    else:
+        ctx = False
+    return index(request, ctx)
